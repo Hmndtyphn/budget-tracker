ca0a8a73776ddd74cbb61993c36f1de180481e5a
HmndTyphn <Jared.elliott09@gmail.com>
2021-12-16 10:24:39 -0600

    save contract offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,17 +6,14 @@ const request = indexedDB.open('budget_tracker', 1);
 
 // event to update database version if applicable
 request.onupgradeneeded = function(event) {
-
     // save to database
     const db = event.target.result;
-
     // create table and set it to auto increment db results with a primary key
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 // triggers if successful 
 request.onsuccess = function(event) {
-
     // saves reference of db to global variable above upon connection
     db = event.target.result;
 
@@ -27,11 +24,20 @@ request.onsuccess = function(event) {
 };
 
 // triggers if error
-request.onerror = function(event) {
-    
+request.onerror = function(event) { 
     // logs the error
     console.log(event.target.errorCode);
 };
+
+// function to save transaction data if currently offline
+function saveRecord(record) {
+    // starts a new transaction
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    // new transction object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    // adds record
+    budgetObjectStore.add(record);
+}
 
 
 
